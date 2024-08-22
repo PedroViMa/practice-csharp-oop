71352abfad1d95771e5d6f63b421e86432ef359c
Pedro Villa <pvillarrealmartinez@deloitte.com>
2024-08-22 15:07:58 -0600

    Classroom and Student classes

---
+++ Classes/Classes/Classroom.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Classes
+{
+    internal class Classroom
+    {
+        private readonly List<Student> Students;
+
+        public Classroom()
+        {
+            Students = new List<Student>();
+        }
+
+        public void AddStudent(Student student)
+        {
+            Students.Add(student);
+        }
+
+        public void RemoveStudent()
+        {
+            Console.WriteLine("Please enter the name of the sutendt to be removed:");
+            string name = Console.ReadLine();
+
+            foreach(Student student in Students)
+            {
+                if(name.Equals(student.Name))
+                {
+                    Students.Remove(student);
+                    return;
+                }
+            }
+
+            Console.WriteLine("None student was removed.");
+        }
+
+        public void ListStudents()
+        {
+            if (!Students.Any())
+            {
+                throw new ArgumentNullException();
+            }
+
+            foreach (Student student in Students)
+            {
+                student.ShowInfo();
+            }
+        }
+    }
+}
---
+++ Classes/Classes/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Classes
+{
+    internal class Student
+    {
+        public string RollNumber { get; }
+        public string Name { get; }
+        public string Grade { get; }
+        private  readonly List<double> marks;
+
+        public Student(string rollNumber, string name, string grade)
+        {
+            RollNumber = rollNumber;
+            Name = name;
+            Grade = grade;
+            marks = new List<double>();
+        }
+
+        public void ShowInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("--------------------")
+                .AppendLine($"Student: {Name}")
+                .AppendLine($"Roll Number: {RollNumber}")
+                .AppendLine($"Grade: {Grade}");
+            Console.Write(sb.ToString());
+        }
+
+        public double AverageGrade()
+        {
+            if (!marks.Any())
+            {
+                throw new ArgumentNullException();
+            }
+
+            return marks.Average();
+        }
+
+        public void AddGrades()
+        {
+            // TODO: method to ask user to insert the student grades.
+        }
+    }
+}
