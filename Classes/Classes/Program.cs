b00ba3d0d791592c888665a5b2bfc5fa0647792e
Pedro Villa <pvillarrealmartinez@deloitte.com>
2024-08-22 15:05:59 -0600

    Adding solution for Classes Exercise

--- Classes/Classes/Program.cs
+++ Classes/Classes/Program.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Classes
+﻿namespace Classes
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Classroom classroom = new Classroom();
+
+            classroom.AddStudent(new Student("00410049", 
+                "Pedro", "9th"));
+            classroom.AddStudent(new Student("00414549",
+                "Juan", "8th"));
+            classroom.AddStudent(new Student("00511249",
+                "Raul", "7th"));
+
+            classroom.ListStudents();
+
+            classroom.RemoveStudent();
+
+            classroom.ListStudents();
         }
     }
 }
